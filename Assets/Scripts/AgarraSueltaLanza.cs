d674c53d6fdaf1e5845a22a9cc7ba184f38c8bb9
Alexoga9 <alexalga32@gmail.com>
2023-12-12 18:13:05 -0400

    Lanzamiento
    -Se sustituyo el script de recogida por uno de recogida y lanzamiento

---
+++ Assets/Scripts/AgarraSueltaLanza.cs
@@ -0,0 +1,76 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AgarraSueltaLanza : MonoBehaviour
+{
+    public GameObject cubo;
+    public Transform mano;
+    public float fuerza;
+
+    private bool activo;
+    private bool enMano;
+    private Vector3 escala;
+
+    private void Start()
+    {
+        //Se registra la escala original del objeto para que no permanezca cambiada al ser recogida
+        escala = cubo.transform.localScale;
+    }
+
+    void Update()
+    {
+        if(activo == true)
+        {
+            if(Input.GetKeyDown(KeyCode.E))
+            {
+                //Se transforma en hijo de mano y se mueve a su posición
+                cubo.transform.SetParent(mano);
+                cubo.transform.position = mano.position;
+                cubo.transform.rotation = mano.rotation;
+                cubo.GetComponent<Rigidbody>().isKinematic = true;
+                enMano = true;
+            }
+        }
+
+        if(Input.GetMouseButtonDown(0))
+        {
+            //El objeto sale del padre y se le aplica una fuerza y su escala original
+            cubo.transform.SetParent(null);
+            cubo.GetComponent<Rigidbody>().isKinematic = false;
+            cubo.transform.localScale = escala;
+
+            if(enMano == true)
+            {
+                cubo.GetComponent<Rigidbody>().AddForce(transform.forward * fuerza, ForceMode.Impulse);
+                enMano = false;
+            }
+        }
+
+        if(Input.GetKeyDown(KeyCode.G))
+        {
+            //El objeto sale del padre y se le aplica su escala original
+            cubo.transform.SetParent(null);
+            cubo.GetComponent<Rigidbody>().isKinematic = false;
+            cubo.transform.localScale = escala;
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        //Trigger de entrada
+        if(other.tag == "Player")
+        {
+            activo = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        //Trigger de salida
+        if (other.tag == "Player")
+        {
+            activo = false;
+        }
+    }
+}
