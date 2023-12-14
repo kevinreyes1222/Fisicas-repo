431dae8a991994ef44ec366fa5eefc5074d9e3c3
Alexoga9 <alexalga32@gmail.com>
2023-12-13 20:41:39 -0400

    Animaciones implementadas
    -Las animaciones se reproducen apropiadamente
    -Añadida animación de Idle
    -Añadida de animación de caminar
    Incorporación de la camara objetos como las manos (objeto texturizado) y mano (GameObject vacio)

--- Assets/Scripts/AnimacionMano.cs
+++ Assets/Scripts/AnimacionMano.cs
@@ -4,15 +4,22 @@ using UnityEngine;
 
 public class AnimacionMano : MonoBehaviour
 {
+     Animator anim;
     // Start is called before the first frame update
     void Start()
     {
-        
+        anim = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.D))
+        {
+            anim.SetFloat("Movimiento", 1f);
+        }
+
+        else
+        { anim.SetFloat("Movimiento", 0f); }
     }
 }
